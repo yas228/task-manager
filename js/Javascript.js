26c74382c025e1db846c41956ddff4e2fe7cd41c
Yawo Siatitse <asiatits@u.rochester.edu>
2019-12-03 23:08:39 +0100

    initial commit

---
+++ js/Javascript.js
@@ -0,0 +1,128 @@
+
+  var cookieContent = "";
+  var logged=false;
+  var cookie="";
+  var cookiel="";
+
+
+  function deleteform(){
+      document.getElementById("user").value="";
+      document.getElementById("name").value="";
+      document.getElementById("surname").value="";
+      document.getElementById("email").value="";
+      document.getElementById("pass").value="";
+      document.getElementById("birth").value="";
+      document.getElementById("terms").value="";
+      document.getElementById("purpose").value="";
+      document.getElementById("interests").value="";
+      document.getElementById("languages").value="";
+
+  }
+  function logout(){
+      document.getElementById("header1").style.display="none";
+      document.getElementById("header2").style.display="inline-block";
+      document.getElementById("username").style.display="none";
+      document.getElementById("welcome1").style.display="none";
+      document.getElementById("welcome0").style.display="none";
+      document.getElementById("logo").src="images/user.png";
+      logged=false;
+      alert("You logged out correctly");
+  }
+  function login(){
+    document.getElementById("login").style.display="block";
+  }
+
+  function register(){
+    document.getElementById("register").style.display="block";
+  }
+
+  function getCookie(cname) {//get the cookie
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for(var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+              c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+              return c.substring(name.length, c.length);
+          }
+      }
+      return "";
+  }
+
+  function setCookie(cname,cvalue,exdays) {//set the cookie
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays*24*60*60*1000));
+      var expires = "expires=" + d.toGMTString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function checkCookie() { //check cookie on register and set it if it does not exist
+       cookie=getCookie(document.getElementById("email").value);
+       if(cookie==""){
+         cookieContent = document.getElementById("email").value+","+document.getElementById("pass").value+","+document.getElementById("user").value+","+document.getElementById("img").value+","+document.getElementById("name").value+","+document.getElementById("surname").value+","+document.getElementById("birth").value+","+document.getElementById("interests").value+","+document.getElementById("languages").value+","+document.getElementById("purpose").value;
+         setCookie(document.getElementById("email").value, cookieContent, 5);
+         deleteform();
+         document.getElementById("register").style.display="none";
+         alert("You have registered correctly!");
+         deleteform();
+       }
+       else{
+         alert("User already exists");
+       }
+     }
+
+    function checkLoginCookie(){ //check cookie on login
+       cookiel=getCookie(document.getElementById("emaillogin").value);
+       if(cookiel==""){
+         alert("Email is not registered.");
+         return;
+       }
+       var content = cookiel.split(",");
+       if(content[1]!=document.getElementById("passlogin").value){
+         alert("Incorrect password.");
+        return;
+       }
+       document.getElementById("HeaderUsername").innerHTML= content[2];
+       document.getElementById("wusername").innerHTML= content[2];
+       document.getElementById("login").style.display="none";
+       document.getElementById("header1").style.display="inline-block";
+       document.getElementById("welcome1").style.display="inline-block";
+       document.getElementById("body").style.display="flex";
+       document.getElementById("header2").style.display="none";
+       document.getElementById("welcome0").style.display="none";
+       document.getElementById("username").style.display="inline-block";
+       if(content[3]!=""){
+         document.getElementById("logo").src=content[3];
+       }
+       logged=true;
+       alert("You have logged in correctly!");
+     }
+
+  function show (elem) {
+    elem.style.display="block";
+  }
+  function hide (elem) {
+    elem.style.display="";
+  }
+
+  window.onclick = function(event) {
+    var share = document.getElementById("sharepopup");
+    var register = document.getElementById("register");
+    var login = document.getElementById("login");
+    var ham1 = document.getElementById("settings2");
+    var ham2 = document.getElementById("settings3");
+    var ham3 = document.getElementById("settings1");
+
+    if (event.target == register) {
+      register.style.display = "none";
+    }
+    if (event.target == login) {
+      login.style.display = "none";
+    }
+
+
+
+  }
